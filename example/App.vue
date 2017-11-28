3d7ee5d936a791a89d24072e97b47d5bd7cef2ff
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2017-11-29 04:05:19 +0530

    :dancers: Added Example

---
+++ example/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Rating v-model="rateMe" type="heart"></Rating>
+  </div>
+</template>
+
+<script>
+import Rating from '../src/v-rating';
+
+export default {
+  components: {
+    Rating
+  },
+  data() {
+  	return {
+  		rateMe: 0
+  	}
+  }
+}
+</script>
